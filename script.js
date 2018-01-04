066a7ae1c568e94db0122ad5a36c81b6db1dc04d
sharadbrat <sharoobrazno@mail.ru>
2018-01-05 01:40:33 +0300

    added smoth carousel images loading

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 function activateCarousel(interval) {
-  var carouselItems = [
-    {
-      image: './resources/carousel_1.jpg'
-    },
-    {
-      image: './resources/carousel_2.jpg'
-    },
-    {
-      image: './resources/carousel_3.jpg'
-    },
-    {
-      image: './resources/carousel_4.jpg'
-    },
-    {
-      image: './resources/carousel_5.jpg'
-    },
-  ];
-
   var carouselWrapper = document.querySelector('main .carousel');
+  var images = carouselWrapper.getElementsByClassName('carousel__image');
   var currentItem = 1;
+  var imagesCount = images.length;
 
   document.getElementById('main-tagline').style.opacity = 1;
 
   return setInterval(function() {
-    carouselWrapper.style.backgroundImage = 'url(\'' + carouselItems[currentItem++].image + '\')';
-    if (currentItem === carouselItems.length)
+    if (currentItem === 0) {
+      images[imagesCount - 1].style.opacity = 0;
+    } else {
+      images[currentItem - 1].style.opacity = 0;
+    }
+    images[currentItem++].style.opacity = 1;
+    if (currentItem === images.length)
       currentItem = 0;
   }, interval || 5000);
 }
