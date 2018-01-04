83f7af72c707f3cd1c422622d726d5289ee90402
sharadbrat <sharoobrazno@mail.ru>
2018-01-05 00:53:31 +0300

    optimized images

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function activateCarousel(interval) {
-	var carouselItems = [
-		{
-			image: './resources/carousel_1.jpg'
-		},
-		{
-	   	image: './resources/carousel_2.jpg'
-		},
-		{
-			image: './resources/carousel_3.jpg'
-		},
+  var carouselItems = [
+    {
+      image: './resources/carousel_1.jpg'
+    },
+    {
+      image: './resources/carousel_2.jpg'
+    },
+    {
+      image: './resources/carousel_3.jpg'
+    },
     {
       image: './resources/carousel_4.jpg'
     },
     {
       image: './resources/carousel_5.jpg'
     },
-	];
+  ];
 
-	var carouselWrapper = document.querySelector('main .carousel');
-	var currentItem = 1;
+  var carouselWrapper = document.querySelector('main .carousel');
+  var currentItem = 1;
 
   document.getElementById('main-tagline').style.opacity = 1;
 
-	return setInterval(function() {
-		carouselWrapper.style.backgroundImage = 'url(\'' + carouselItems[currentItem++].image + '\')';
-		if (currentItem === carouselItems.length)
-			currentItem = 0;
-	}, interval || 5000);
+  return setInterval(function() {
+    carouselWrapper.style.backgroundImage = 'url(\'' + carouselItems[currentItem++].image + '\')';
+    if (currentItem === carouselItems.length)
+      currentItem = 0;
+  }, interval || 5000);
 }
 
 
 window.onload = function main() {
-	activateCarousel();
+  activateCarousel();
 };
