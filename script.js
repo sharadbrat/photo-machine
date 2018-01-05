1a814831fd529bf4d8564963bfab85549ee6dde5
sharadbrat <sharoobrazno@mail.ru>
2018-01-05 13:06:46 +0300

    added animation and some optimizations

--- script.js
+++ script.js
@@ -18,7 +18,25 @@ function activateCarousel(interval) {
   }, interval || 5000);
 }
 
+function activateScroll() {
+  var aboutGroups = document.getElementsByClassName('about__group');
+  var windowHeight = window.innerHeight;
+  window.onscroll = function() {
+    (function() {
+      for (var i = aboutGroups.length - 1; i >= 0; i--) {
+        var rect = aboutGroups[i].getBoundingClientRect();
+        if (rect.top < (windowHeight - rect.height) / 2 + windowHeight / 5) {
+          aboutGroups[i].style.transform = 'translateX(0)';
+          aboutGroups[i].style.opacity = 1;
+        }
+      }
+    })();
+    if (aboutGroups[aboutGroups.length - 1].style.opacity == 1)
+      window.onscroll = undefined;
+  }
+}
 
 window.onload = function main() {
   activateCarousel();
+  activateScroll();
 };
